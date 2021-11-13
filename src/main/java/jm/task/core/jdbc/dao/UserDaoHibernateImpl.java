d3b590409ea567322f97c4fbfb57f88c84d9346b
Pavel Porchuk <89536640+VeryBigPahan@users.noreply.github.com>
2021-11-13 04:09:27 +0300

    JDBC Hibernate Initial Commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,7 +1,12 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -12,31 +17,60 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        Session session = Util.HibernateConnect().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.createSQLQuery("CREATE TABLE IF NOT EXISTS `user` (\n" +
+                "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                "  `name` VARCHAR(45) NULL,\n" +
+                "  `lastname` VARCHAR(45) NULL,\n" +
+                "  `age` INT NULL,\n" +
+                "  PRIMARY KEY (`id`));").executeUpdate();
+        tx1.commit();
+        session.close();
     }
 
     @Override
     public void dropUsersTable() {
-
+        Session session = Util.HibernateConnect().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.createSQLQuery("DROP TABLE IF EXISTS user").executeUpdate();
+        tx1.commit();
+        session.close();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Session session = Util.HibernateConnect().openSession();
+        Transaction tx1 = session.beginTransaction();
+        User user = new User(name, lastName, age);
+        session.save(user);
+        tx1.commit();
+        session.close();
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Session session = Util.HibernateConnect().openSession();
+        Transaction tx1 = session.beginTransaction();
+        User user = session.get(User.class, id);
+        session.delete(user);
+        tx1.commit();
+        session.close();
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        String query = "FROM User";
+        TypedQuery<User> typedQuery = Util.HibernateConnect().openSession().createQuery(query, User.class);
+        return typedQuery.getResultList();
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Session session = Util.HibernateConnect().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.createSQLQuery("TRUNCATE user").executeUpdate();
+        tx1.commit();
+        session.close();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,9 +1,18 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     public static final String URL = "jdbc:mysql://localhost:3306/jmdatabase";
@@ -30,6 +39,29 @@ public class Util {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public static SessionFactory HibernateConnect() {
+        Properties prop = new Properties();
+        prop.setProperty("hibernate.connection.url","jdbc:mysql://localhost:3306/jmdatabase");
+        prop.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+        prop.setProperty("hibernate.connection.username","root");
+        prop.setProperty("hibernate.connection.password","root");
+        prop.setProperty("hibernate.show_sql", "false");
+
+        Configuration cfg = new Configuration();
+        cfg.setProperties(prop);
+        cfg.addAnnotatedClass(User.class);
+
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(prop).build();
+
+        return cfg.buildSessionFactory(serviceRegistry);
+    }
+
+    public static void HibernateCloseConnect(Session session) {
+        if (session != null) {
+            session.close();
         }
     }
 }
